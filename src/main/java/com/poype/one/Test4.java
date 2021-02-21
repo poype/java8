176e48c14f739e24b403cd2ccef76028e5e1f878
Poype <253186926@qq.com>
2021-02-21 11:02:34 +0800

    如果一个类同时实现两个interface，且这两个interface都提供了同一个方法的默认实现 那这个类就不知道该使用哪个实现，为了解决冲突，只能自己提供一个精确的实现

--- src/main/java/com/poype/one/Test4.java
+++ src/main/java/com/poype/one/Test4.java
@@ -22,7 +22,22 @@ interface Speak {
     }
 }
 
-// Worker的父类和实现的接口中都提供了相同方法的实现，super class win。interface接口中的默认实现被忽略
-class Worker extends Person implements Speak {
+interface Run {
 
+    default void getDescription() {
+        System.out.println("run run run run !!!");
+    }
+}
+
+// Worker的父类和实现的接口中都提供了相同方法的实现，super class win。interface接口中的默认实现被忽略
+class Worker implements Speak, Run {
+
+    /**
+     * Speak和Run两个接口中的getDescription方法都有默认实现，Worker类不知道该使用哪个实现
+     * 为了解决冲突，只能自己提供一个精确的实现
+     */
+    @Override
+    public void getDescription() {
+        System.out.println("Worker类自己的getDescription方法");
+    }
 }
