0d35ac7406c32c4dccc43d7fbe896901cd80d045
Poype <253186926@qq.com>
2021-02-23 03:48:54 +0800

    1. limit 2. skip 3. Stream.concat

--- src/main/java/com/poype/two/Test3.java
+++ src/main/java/com/poype/two/Test3.java
@@ -2,6 +2,7 @@ package com.poype.two;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Test3 {
 
@@ -16,6 +17,12 @@ public class Test3 {
 
     public static void main(String[] args) {
         transform();
+        System.out.println("---------------------");
+        limitN();
+        System.out.println("---------------------");
+        skipN();
+        System.out.println("---------------------");
+        concat();
     }
 
     // map方法的interface function接收一个参数，生成一个结果
@@ -30,5 +37,23 @@ public class Test3 {
         wordList.stream()
                 .map((str) -> str.charAt(0))
                 .forEach(System.out::println);
+    }
+
+    private static void limitN() {
+        // The call stream.limit(n) returns a new stream that ends after n elements
+        // (or when the original stream ends if it is shorter).
+        wordList.stream().limit(3).forEach(System.out::println);
+    }
+
+    private static void skipN() {
+        // 跳过前3给元素
+        wordList.stream().skip(3).forEach(System.out::println);
+    }
+
+    // Stream.concat静态方法拼接两个stream
+    private static void concat() {
+        Stream<Integer> firstStream = Stream.of(1, 2, 3, 4);
+        Stream<Integer> secondStream = Stream.of(5, 6, 7, 8);
+        Stream.concat(firstStream, secondStream).forEach(System.out::println);
     }
 }
